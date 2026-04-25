9b2dd90a284ea66bd6a8a47ceedad44473509e6c
David Lago <david@macbook-pro.myfiosgateway.com>
2026-04-24 22:16:20 -0400

    Did two beginner exercises of counting and addition

---
+++ addition.py
@@ -0,0 +1,5 @@
+# Adding a list of numbers from 1 to 100 and printing the total.
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
---
+++ counting.py
@@ -0,0 +1,4 @@
+# Making a list of numbers from 1 to 10 and printing each number in the list.
+count = range(1, 11)
+for count in count:
+    print(count)
